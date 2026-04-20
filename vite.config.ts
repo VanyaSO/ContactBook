d9f6ca904143c01c40e916f594c5152b1b6ff4dd
Ivan Ushachov <ushachovg324@gmail.com>
2026-04-20 18:43:03 +0300

    add updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 import path from "path";
 
 export default defineConfig({
+  base: '/ContactBook/',
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  plugins: [react() as any],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
